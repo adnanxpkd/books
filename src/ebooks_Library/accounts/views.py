9a99f9f5fcf23572f45c5e10f65b7d8badcfd09e
мυнαммє∂ α∂ηαη к <73103641+adnanxpkd@users.noreply.github.com>
2025-09-13 14:36:55 +0530

    3

--- src/ebooks_Library/accounts/views.py
+++ src/ebooks_Library/accounts/views.py
@@ -3,17 +3,12 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.shortcuts import redirect, render
 
-
-
-# Create your views here.
-
-
-def Signout_View(request):
-    if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
-        if form.is_valid():
-            user =form.get_user()
-            login(request, user)
-            return redirect('home')
-    else:
-        form = AuthenticationForm()
+# def Signout_View(request):
+#     if request.method == 'POST':
+#         form = UserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             user =form.get_user()
+#             login(request, user)
+#             return redirect('home')
+#     else:
+#         form = AuthenticationForm()
